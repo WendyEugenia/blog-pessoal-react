d89bf487612634100d8fff65889394853c65bd28
WendyEugenia <wendy.t.n@hotmail.com>
2024-01-17 23:29:06 -0300

    add rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import './App.css';
-import Home from './Home/pages/Home';
+import { BrowserRouter } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import Login from './pages/login/Login'
+
 
 function App() {
   return (
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,14 +1,15 @@
 function Grid() {
   return (
     <div className="grid grid-cols-12">
-        <div className="col-span-1 bg-red-300 h-screen">Servidores</div>
-        <div className="col-span-3 bg-green-300 h-screen" >Canais</div>
-        <div className="col-span-6 bg-yellow-300 -300 h-screen">Feed Noticias</div>
-        <div className="col-span-2 bg-blue-300 h-screen">Área de Membros</div>
-    </div>
-  )
-    
+      <div className="col-span-1 bg-red-300 h-screen">Servidores</div>
 
+      <div className="col-span-3 bg-blue-300 h-screen">Canais</div>
+
+      <div className="col-span-6 bg-orange-300 h-screen">Feed de Mensagens</div>
+
+      <div className="col-span-2 bg-green-300 h-screen">Area de Memes</div>
+    </div>
+  );
 }
 
-export default Grid
+export default Grid;
