4e110cf4c20ff65e180d60038e1282c5701fd30b
WendyEugenia <wendy.t.n@hotmail.com>
2024-01-31 10:10:04 -0300

    corrigindo o alert 2

--- src/components/temas/deletarTemas/DeletarTema.tsx
+++ src/components/temas/deletarTemas/DeletarTema.tsx
@@ -3,62 +3,43 @@ import { useNavigate, useParams } from 'react-router'
 import { AuthContext } from '../../../contexts/AuthContext'
 import Tema from '../../../models/Tema'
 import { buscar, deletar } from '../../../services/Service'
-import { RotatingLines } from 'react-loader-spinner'
+import { toastAlerta } from '../../../utils/toastAlerta'
 
 function DeletarTema() {
+    const [tema, setTema] = useState<Tema>({} as Tema)
 
-    // Variavel de Estado de Carregamento - usada para indicar que está havendo alguma requisição ao Back
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+    let navigate = useNavigate()
 
-    // Variavel de Estado de Tema - Registra um Objeto da Model Tmea. Usada para armazena os dados que foram digitados nos inputs do formulario
-    const [tema, setTema] = useState<Tema>({} as Tema)    // Iniciamos um objeto vazio da Model Tema
+    const { id } = useParams<{ id: string }>()
 
-    // Criamos uma constante que recebe o hook useNavigate, para podermos redirecionar o usuário
-    const navigate = useNavigate()
-
-    // useParams = Esse hook serve para pegarmos parametros que veem na url do FRONT
-    const { id } = useParams<{ id: string }>()  // Aqui, pegamos da URL um parametro/variavel chamado ID. Veja a rota de editarTema no APP.tsx 
-
-    // Pega as informações que queremos do nosso Contexto através do hook useContexto
     const { usuario, handleLogout } = useContext(AuthContext)
-    const token = usuario.token;    // pega o Token do Objeto Usuario
+    const token = usuario.token
 
-    // Função que vai chamada a service de Buscar para buscarmos um Tema em Especifico
     async function buscarPorId(id: string) {
         try {
-
-            await buscar(`/temas/${id}`, setTema, { // Aqui, na URL de Requisição, passamos o ID do Tema a ser Buscado
+            await buscar(`/temas/${id}`, setTema, {
                 headers: {
-                    'Authorization': token          // Passando um token pelo atributo Authorization
+                    'Authorization': token
                 }
             })
-
         } catch (error: any) {
-
-            if (error.toString().includes('403')) {                 // Verifica se o erro é o 403 - Proibido que indica que o Token Expirou
-                alert('O token expirou, favor logar novamente')     // Avisa ao usuário que deu ruim
-                handleLogout()                                      // Chama a função para deslogar o usuário
+            if (error.toString().includes('403')) {
+                toastAlerta('O token expirou, favor logar novamente', 'info')
+                handleLogout()
             }
         }
     }
 
-    // Função de Efeito Colateral - Sempre que a variavel token, tiver o seu valor alterado
-    // uma função  é disparada, essa função verifica se o token é IDÊNTICO a "", se sim, isso indica que o usuário NÃO ESTÁ LOGADO.
-    // Com isso, o avisamos e enviamos para a tela de Login
     useEffect(() => {
         if (token === '') {
-            alert('Você precisa estar logado')
+            toastAlerta('Você precisa estar logado', 'info')
             navigate('/login')
         }
     }, [token])
 
-
-    // Função de Efeito Colateral - Sempre que o ID for montado pelo React dentro do Componente,
-    //  uma função é disparada, iremos verificar se o ID é diferente de undefined, se sim, quer dizer que iremos atualizar um Tema, 
-    // por isso, precisamos chamar a função que irá fazer uma requisição ao back para carregar os dados do Tema em tela
     useEffect(() => {
         if (id !== undefined) {
-            buscarPorId(id) // esse ID, é o que vem pela a URL da rota do Front End
+            buscarPorId(id)
         }
     }, [id])
 
@@ -66,21 +47,18 @@ function DeletarTema() {
         navigate("/temas")
     }
 
-    // Função assincrona que vai deletar o tema
     async function deletarTema() {
-        setIsLoading(true)
-
         try {
-            await deletar(`/temas/${id}`, { // Aqui, na URL de Requisição, passamos o ID do Tema a ser Excluído
+            await deletar(`/temas/${id}`, {
                 headers: {
-                    'Authorization': token  // Passando um token pelo atributo Authorization
+                    'Authorization': token
                 }
             })
 
-            alert('Tema apagado com sucesso')
+            toastAlerta('Tema apagado com sucesso', 'sucesso')
 
         } catch (error) {
-            alert('Erro ao apagar o Tema')
+            toastAlerta('Erro ao apagar o Tema', 'erro')
         }
 
         retornar()
@@ -96,18 +74,8 @@ function DeletarTema() {
                 <p className='p-8 text-3xl bg-slate-200 h-full'>{tema.descricao}</p>
                 <div className="flex">
                     <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar}>Não</button>
-
                     <button className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-600 flex items-center justify-center' onClick={deletarTema}>
-                        {isLoading ?
-                            <RotatingLines
-                                strokeColor="white"
-                                strokeWidth="5"
-                                animationDuration="0.75"
-                                width="24"
-                                visible={true}
-                            /> :
-                            <span>Sim</span>
-                        }
+                        Sim
                     </button>
                 </div>
             </div>
