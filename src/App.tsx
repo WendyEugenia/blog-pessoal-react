b1579ee288827401a9cdc898b662a9553ed54635
WendyEugenia <wendy.t.n@hotmail.com>
2024-01-31 10:41:28 -0300

    corrigindo

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { ToastContainer } from 'react-toastify'
 
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import Perfil from './pages/Perfil/Perfil'
-import Cadastro from './pages/Cadastro/Cadastro'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Perfil from './pages/perfil/Perfil'
+import Cadastro from './pages/cadastro/Cadastro'
 import ListaTemas from './components/temas/listaTemas/ListaTemas'
 import FormularioTema from './components/temas/formularioTema/FormularioTema'
-import DeletarTema from './components/temas/deletarTemas/DeletarTema'
+import DeletarTema from './components/temas/deletarTema/DeletarTema'
 import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens'
 import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem'
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem'
@@ -31,13 +31,31 @@ function App() {
                         <Route path="/cadastro" element={<Cadastro />} />
                         <Route path="/home" element={<Home />} />
                         <Route path="/temas" element={<ListaTemas />} />
-                        <Route path="/cadastroTema" element={<FormularioTema />} />
-                        <Route path="/editarTema/:id" element={<FormularioTema />} />   {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser editado */}
-                        <Route path="/deletarTema/:id" element={<DeletarTema />} />     {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser excluído */}
-                        <Route path="/postagens" element={<ListaPostagens />} />
-                        <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
-                        <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
-                        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+                        <Route 
+                          path="/cadastroTema" 
+                          element={<FormularioTema />} 
+                        />
+                        <Route 
+                          path="/editarTema/:id" 
+                          element={<FormularioTema />} 
+                        />   {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser editado */}
+                        <Route 
+                          path="/deletarTema/:id" 
+                          element={<DeletarTema />} 
+                        />     {/* :id = é uma variavel que vem pela url do Front, que represenda o id do item que vai ser excluído */}
+                        <Route path="/postagens" element={<ListaPostagens />} 
+                        />
+                        <Route 
+                          path="/cadastroPostagem" 
+                          element={<FormularioPostagem />} 
+                        />
+                        <Route 
+                          path="/editarPostagem/:id" 
+                          element={<FormularioPostagem />} 
+                        />
+                        <Route 
+                        path="/deletarPostagem/:id" 
+                        element={<DeletarPostagem />} />
                         <Route path="/perfil" element={<Perfil />} />
                     </Routes>
                 </div>
